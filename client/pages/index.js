6ded2c0826816a93eefade78bfd194bea3d925d4
Nika <nikushapanchulidze04@gmail.com>
2025-03-08 00:24:18 +0400

    code update

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,16 @@
-function index() {
+import axios from "axios";
+
+export async function getServerSideProps() {
+  // const response = await axios.get("/api/users/currentuser");
+ 
+  console.log("I was executed")
+  // return { props: { currentUser: response.data } }
+  return { props: { } }
+}
+
+function LandingPage({ currentUser }) {
+  // console.log(currentUser)
+  
   return (
     <h1>
       Landing page
@@ -6,4 +18,4 @@ function index() {
   )
 }
 
-export default index
+export default LandingPage
