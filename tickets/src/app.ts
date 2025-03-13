bbac2230c6eca25f4a8587bc0908ead5697cc2ea
Nika <nikushapanchulidze04@gmail.com>
2025-03-13 17:53:50 +0400

    code update

---
+++ tickets/src/app.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@npticketing/common";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: process.env.NODE_ENV !== "test"
+}))
+
+app.all("*", async () => {
+  throw new NotFoundError()
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(err, req, res, next);
+});
+
+export { app };
