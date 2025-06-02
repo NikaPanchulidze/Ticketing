dbc8cc744e313a96594bd4e75565067b303e596d
Nika <nikushapanchulidze04@gmail.com>
2025-06-02 12:08:46 +0400

    fix test problems

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -141,6 +141,8 @@ it("rejects updates if the ticket is reserved", async () => {
   const ticket = await Ticket.findById(response.body.id);
   ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() });
 
+  await ticket!.save();
+
   await request(app)
     .put(`/api/tickets/${response.body.id}`)
     .set("Cookie", cookie)
